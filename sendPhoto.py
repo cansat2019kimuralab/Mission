71c0759e7c5d534cc72089d3807da91e3b563e41
masatoshiMoto <k11000660@gmail.com>
2019-08-16 09:57:24 +0900

    20190816

--- sendPhoto.py
+++ sendPhoto.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("home/pi/kimuralab/Mission")
+sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/IM920')
 import cv2
 import time
 import numpy as np
@@ -10,6 +10,21 @@ img = cv2.imread("/home/pi/photo/photo21.jpg")
 img = cv2.resize(img, (160, 120))
 cv2.imwrite('sendPhoto.jpg', img)
 
-for i in range(len(hide)):
-    for j in range(len(hide[i])):
-        IM920.Send(str(i) + ":" + str(j) + ":" + str(hide[i][j][0]) + ":" + str(hide[i][j][1] + ":" +  str(hide[i][j][2]))
+num = 1000000
+IM920.Send(str(num))
+print(num)
+
+
+print("Start")
+
+'''
+for i in range(len(img)):
+	for j in range(len(img[i])):
+		num = int(img[i][j][0]) + j * 1000 + i * 1000000
+		IM920.Send(str(num))
+		print(num, i, j, img[i][j][0])
+		#time.sleep(0.05)
+	#print(i)
+'''
+
+print("Finish")
