f97d7053784a723124b7a9a4516a71505edfb0ac
masatoshiMoto <k11000660@gmail.com>
2019-08-17 21:41:39 +0900

    20190817

--- sendPhoto.py
+++ sendPhoto.py
@@ -1,16 +1,21 @@
 import sys
 sys.path.append('/home/pi/git/kimuralab/SensorModuleTest/IM920')
+sys.path.append('/home/pi/git/kimuralab/Other')
 import cv2
 import time
 import numpy as np
 
 import IM920
+import Other
 
+sendPhotoPath = '/home/pi/photo/sendPhoto'
+sendPhotoName = ''
 
 def sendPhoto(photoPath):
 	img = cv2.imread(photoPath)
 	img = cv2.resize(img, (80, 60))
-	cv2.imwrite('sendPhoto.jpg', img)
+	#sendPhotoName = Other.fileName(sendPhotoPath, 'jpg')
+	#cv2.imwrite(sendPhotoName, img)
 
 	#print("Start")
 	t = time.time()
@@ -19,14 +24,18 @@ def sendPhoto(photoPath):
 			num = int(img[i][j][2]) + int(img[i][j][1]) * (10 ** 3) + int(img[i][j][0]) * (10 ** 6) + j * (10 ** 9) + i * (10 ** 12)
 			IM920.Send(str(num))
 			print(num)
-	IM920.Send("P1F")
-	IM920.Send("P2S")
 	IM920.Send("MF")
 	IM920.Send("MF")
 	IM920.Send("MF")
-	print(time.time() - t)
-	print("Finish")
+	#print(time.time() - t)
+	#print("Finish")
 
 if __name__ == "__main__":
 	photoName = "/home/pi/photo/photo21.jpg"
 	sendPhoto(photoName)
+	'''
+	while 1:
+		for i in range(10):
+			IM920.Send("PPPPPPPPPP" + str(i))		
+			print(i)
+	'''
